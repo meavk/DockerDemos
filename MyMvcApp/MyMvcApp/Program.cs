537314c04e719db7f77ea22e91c7919c974719a4
Arun V Kumar <arunvkumar7@live.com>
2018-08-07 18:59:48 +0530

    Using port number 5000

--- MyMvcApp/MyMvcApp/Program.cs
+++ MyMvcApp/MyMvcApp/Program.cs
@@ -20,6 +20,7 @@ namespace MyMvcApp
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://*:5000")
                 .Build();
     }
 }
