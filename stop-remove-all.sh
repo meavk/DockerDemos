0797fec885ced9c3110ae2bf1d9836862fd4ec67
Ubuntu <arunv@DockerDemo.0u11ypunka3ebla0yzjaxxfpof.cx.internal.cloudapp.net>
2018-08-12 18:01:52 +0000

    Added script to remove all

---
+++ stop-remove-all.sh
@@ -0,0 +1,11 @@
+docker container stop mvc-step-1
+docker container stop mvc1
+docker container stop mvc2
+docker container stop redisdb
+docker container stop proxy
+docker container rm mvc-step-1
+docker container rm mvc1
+docker container rm mvc2
+docker container rm redisdb
+docker container rm proxy
+
